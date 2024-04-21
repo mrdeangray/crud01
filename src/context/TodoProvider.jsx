e3bc36fb028e9453485060e8bacbf04c81b79487
mrdeangray <mrdeangray@gmail.com>
2024-04-21 08:51:43 -0400

    added CRUD operations

--- src/context/TodoProvider.jsx
+++ src/context/TodoProvider.jsx
@@ -1,16 +1,19 @@
-import React, {useState, createContext} from 'react'
+import React, { useState, createContext, useEffect } from "react";
 
+export const TodoContext = createContext(null);
+const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
 
-export const TodoContext = createContext(null)
-const TodoProvider = ({children}) => {
-const [todos, setTodos] = useState(["aaa", "bbb", "ccc"])
+  useEffect(() => {
+    const savedTodos = JSON.parse(window.localStorage.getItem("todos1")) || [];
+    setTodos(savedTodos);
+  }, []);
 
   return (
-    <TodoContext.Provider value={{todos, setTodos}}>
-        {children}
+    <TodoContext.Provider value={{ todos, setTodos }}>
+      {children}
     </TodoContext.Provider>
-  
-  )
-}
+  );
+};
 
-export default TodoProvider
+export default TodoProvider;
